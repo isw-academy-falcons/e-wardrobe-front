996c5e6996a187cb6f799fa5d6afd621028d2a83
Michael Olowoyo <Michael.Olowoyo@Interswitchng.com>
2023-09-02 07:21:42 +0100

    feature/homescreen page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import './App.css';
+import Homescreen from './pages/Homescreen';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homescreen/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/*' element={<PageNotFound/>}/>
+        </Routes>
+      </Router>     
     </div>
   );
 }
