9195ee913b0d796a436d4be4378e6374ea96e34a
Michael Olowoyo <Michael.Olowoyo@Interswitchng.com>
2023-09-02 19:48:41 +0100

    created the page not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className=' text-2xl bg-[#FCDE7C] flex min-h-screen'>
+      <h2 className='mx-auto my-4 text-black font-bold'>PAGE NOT FOUND</h2> 
+
+    </div>
   )
 }
 
