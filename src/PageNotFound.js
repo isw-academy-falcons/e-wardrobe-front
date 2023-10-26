f59418d6927fcd9dd3f8e40bea31bfb7a3626e28
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-10-26 11:35:38 +0000

    chore: Cleanup PageNotfound.js

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PageNotFound = () => {
   return (
     <div className=' text-2xl bg-[#FCDE7C] flex min-h-screen'>
-      <h2 className='mx-auto my-4 text-black font-bold'>PAGE NOT FOUND</h2> 
+      <h2 className='mx-auto my-4 text-black font-bold'>PAGE NOT FOUND</h2>
     </div>
   )
 }
